01067375b289c1063c3a92ea2cb07fcf0d4a6aaf
tirthp139 <tirth13919patel@gmail.com>
2021-11-23 00:38:03 -0800

    Fixed wrapper to calculate memory and time

--- calcMemoryTime.py
+++ calcMemoryTime.py
@@ -1,24 +1,23 @@
 import time
-from memory_profiler import memory_usage
-from multiprocessing import freeze_support
-
+import tracemalloc
 
 def run(func, *args):
-    freeze_support()
+    tracemalloc.start()
     start = time.time()
-    mem_usage = memory_usage((func, args), interval=0.2)
-    print(max(mem_usage))
-    return (time.time()-start), max(mem_usage)
+    func(*args)
+    current, peak = tracemalloc.get_traced_memory()
+    tracemalloc.stop()
+    return time.time()-start, peak/10**6
 
 # def x(a,b=10,c=20):
 #     print(a,b,c)
 #     ans = []
-#     for i in range(10000):
+#     for i in range(100):
 #         for j in range(30):
 #             for k in range(100):
 #                 ans.append((a,b,c))
 #     return ans
 #
-#
+
 # if __name__ == '__main__':
 #     print(run(x, 5,4,3))
