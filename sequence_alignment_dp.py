167afb073bb8f05abaf6465c6c25e793a701cd38
tirthp139 <tirth13919patel@gmail.com>
2021-11-24 11:10:35 -0800

    Rollback

--- sequence_alignment_dp.py
+++ sequence_alignment_dp.py
@@ -1,82 +1,85 @@
 s1 = 'ACACACTGACTACTGACTGGTGACTACTGACTGGACTGACTACTGACTGGTGACTACTGACTGG'
-# s2 = 'TATTATTATACGCTATTATACGCGACGCGGACGCGTATACGCTATTATACGCGACGCGGACGCG'
-s2 = 'A'
+s2 = 'ACA'
+# s1 = 'ATGGCCTC'
+# s2 = 'ACGGCTC'
+# s1 = 'AGGGCT'
+# s2 = 'AGGCA'
+# s1 = 'ACACACTGACTACTGACTGGTGACTACTGACTGGACTGACTACTGACTGGTGACTACTGACTGG'
+# s2 = 'TATTATACGCGACGCG'
+
+
 mismatch_penalty = [[0, 110, 48, 94], [110, 0, 118, 48], [48, 118, 0, 110], [94, 48, 110, 0]]
+# mismatch_penalty = [[0, 3, 3, 3], [3, 0, 3, 3], [3, 3, 0, 3], [3, 3, 3, 0]]
+# mapping characters to numbers in order to access their respective mismatch penalties
+mismatch_dict = {'A':0, 'C':1, 'G':2, 'T':3}
 gap_penalty = 30
 
 
 def seq_align(s1, s2, gap_penalty, mismatch_penalty):
-    m = len(s1)
-    n = len(s2)
+    length_s1 = len(s1)
+    length_s2 = len(s2)
 
-    A = [[0 for i in range(n + 1)] for j in range(m + 1)]  # initializing the optimal value matrix
+    A = [[0 for i in range(length_s2 + 1)] for j in range(length_s1 + 1)]  #initializing the optimal value matrix
 
-    for i in range(m + 1):  # initializing the first row and column with gap penalties
+    for i in range(length_s1):              #initializing the first row and column with gap penalties
         A[i][0] = i * gap_penalty
-
-    for i in range(n + 1):
+    for i in range(length_s2):
         A[0][i] = i * gap_penalty
 
-    mismatch_dict = {'A': 0, 'C': 1, 'G': 2, 'T': 3}  # mapping characters to numbers in order to access their respective mismatch
-                                                      # penalties
-
-    for i in range(1, m + 1):  # parsing over both the strings in order to find minimalize the penalty
-        for j in range(1, n + 1):
-            if s1[i - 1] != s2[j - 1]:  # finding optimal penalty from mismatch and gap penalties
-                A[i][j] = min(mismatch_penalty[mismatch_dict[s1[i - 1]]][mismatch_dict[s2[j - 1]]] + A[i - 1][j - 1],
-                              gap_penalty + A[i - 1][j], gap_penalty + A[i][j - 1])
+    for j in range(1,length_s2+1):          #parsing over both the strings in order to find minimalize the penalty
+        for i in range(1,length_s1+1):
+            if s1[i-1] != s2[j-1]:          #finding optimal penalty from mismatch and gap penalties
+                A[i][j] = min(mismatch_penalty[mismatch_dict[s1[i-1]]][mismatch_dict[s2[j-1]]] + A[i-1][j-1],
+                              gap_penalty + A[i-1][j], gap_penalty + A[i][j-1])
             else:
-                A[i][j] = A[i - 1][j - 1]
-    print(A)
-    i = m - 1
-    j = n - 1
+                A[i][j] = A[i-1][j-1]
+    getBackTraceHelper(A, s1, s2, gap_penalty, mismatch_penalty)
+    print(A[-1][-1])
 
-    x_final = []
-    y_final = []
 
-    while not (i == 1 or j == 1):
-        if s1[i - 1] == s2[j - 1]:
-            x_final.append(s1[i - 1])
-            y_final.append(s2[j - 1])
-            i -= 1
-            j -= 1
+def getBackTraceHelper(A, s1, s2, gap_penalty, mismatch_penalty):
+    row = len(A) - 1
+    col = len(A[0]) - 1
+    res_s1 = ''
+    res_s2 = ''
 
-        elif (A[i - 1][j - 1] + mismatch_penalty[mismatch_dict[s1[i - 1]]][mismatch_dict[s2[j - 1]]]) == A[i][j]:
-            x_final.append(s1[i - 1])
-            y_final.append(s2[j - 1])
-            i -= 1
-            j -= 1
+    while row >= 1 and col >= 1:
+        pen_mismatch = A[row][col] - A[row-1][col-1]
+        pen_gap_s1 = A[row][col] - A[row][col-1]
+        pen_gap_s2 = A[row][col] - A[row-1][col]
+        row_char = s1[row - 1]
+        col_char = s2[col - 1]
 
-        elif (A[i - 1][j] + gap_penalty) == A[i][j]:
-            x_final.append(s1[i - 1])
-            y_final.append('_')
-            i -= 1
 
-        elif (A[i][j - 1] + gap_penalty) == A[i][j]:
-            x_final.append('_')
-            y_final.append(s2[j - 1])
-            j -= 1
+        # Cond 3
+        if pen_mismatch == mismatch_penalty[mismatch_dict[row_char]][mismatch_dict[col_char]]:
+            res_s1 = row_char + res_s1
+            res_s2 = col_char + res_s2
+            row -= 1
+            col -= 1
 
-    x_final = ''.join(x_final[::-1])
-    y_final = ''.join(y_final[::-1])
+        # Cond 1
+        elif pen_gap_s1 == gap_penalty:
+            res_s1 = '_' + res_s1
+            res_s2 = col_char + res_s2
+            col -= 1
 
-    print(i, j)
-    if i != 0:
-        x_final = s1[:i] + x_final
-        y_final = '_'*i + y_final
-    if j != 0:
-        x_final = '_'*j + x_final
-        y_final = s2[:j] + y_final
-    # x_string = ''.join(x_final)
-    # y_string = ''.join(y_final)
+        # Cond 2
+        elif pen_gap_s2 == gap_penalty:
+            res_s2 = '_' + res_s2
+            res_s1 = row_char + res_s1
+            row -= 1
 
-    # print(x_final[:50])
-    # print(x_final[-50:])
-    #
-    # print(y_final[:50])
-    # print(y_final[-50:])
-    return x_final, y_final
+    if row != 1:
+        res_s1 = s1[:row] + res_s1
+        res_s2 = ('_' * row) + res_s2
 
-print(seq_align( s1, s2, gap_penalty, mismatch_penalty))
-# # if __name__ == '__main__':
-# #     print(calcMemoryTime.run(seq_align, s1, s2, gap_penalty, mismatch_penalty))
+    if col != 1:
+        res_s2 = s2[:col] + res_s2
+        res_s1 = ('_' * col) + res_s1
+    # print(res_s1[:50], res_s2[:50])
+    # print(res_s1[-50:], res_s2[-50:])
+    print(res_s1)
+    print(res_s2)
+
+seq_align(s1, s2, gap_penalty, mismatch_penalty)
